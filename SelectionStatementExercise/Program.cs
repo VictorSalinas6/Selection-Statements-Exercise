2a819feb8983249ce3d680547a28f82768ca8688
Victor Salinas <victorsalinas6@gmail.com>
2023-12-12 10:41:04 -0600

    Did as requested

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This is a game that you have to guess wich number I´m thinking. n");
+            Console.WriteLine("Please enter your first guess.");
+            var userInput = int.Parse(Console.ReadLine());
+
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+
+            if (userInput == favNumber)
+                Console.WriteLine("You guessed it!!!");
+
+            else if (userInput < favNumber)
+                Console.WriteLine("Too low.");
+
+            else
+                Console.WriteLine("Too high");
+
         }
     }
 }
